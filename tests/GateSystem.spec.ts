6479ad49db183fb54162ed613a80740c7504f124
dkolot <dkolotsoftserveinc.com>
2022-01-28 17:41:36 +0200

    update GateSystem test

--- tests/GateSystem.spec.ts
+++ tests/GateSystem.spec.ts
@@ -1,7 +1,24 @@
 import GateSystem from '../src/ts/GateSystem';
+import GateSensor from '../src/ts/GateSensor';
+import GateController from '../src/ts/GateController';
+import GateInterface from '../src/ts/GateInterface';
+
+jest.mock('../src/ts/GateSensor');
+jest.mock('../src/ts/GateController');
+jest.mock('../src/ts/GateInterface');
 
 describe('GateSystem', () => {
-  const gateSystem = new GateSystem();
+  let gateSystem: Object;
+
+  beforeEach(() => {
+    const controller = new GateController();
+
+    gateSystem = {
+      sensor: new GateSensor(),
+      controller: controller,
+      interface: new GateInterface(controller),
+    };
+  });
 
   it('Property sensor exists', () => {
     expect(gateSystem).toHaveProperty('sensor');
