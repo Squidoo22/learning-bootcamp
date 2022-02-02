6df1af44e9a4ccf427c47039d2036e235f68775e
Igbin <igbin87@gmail.com>
2022-02-02 11:34:20 +0200

    Added MessagesController test

---
+++ tests/MessagesController.spec.ts
@@ -0,0 +1,21 @@
+import messagesController from '../src/ts/MessagesController';
+
+describe('MessagesController', () => {
+  const testMessage = 'test';
+  console.log = jest.fn();
+
+  it('Property notifications exists', () => {
+    expect(messagesController).toHaveProperty('notifications');
+  });
+
+  it('Method showNotification works properly', () => {
+    messagesController.showNotification(testMessage);
+    expect(console.log).toBeCalledWith(`%c ${testMessage} `, "background: #222; color: #bada55");
+  });
+
+  it('Method showNotificationsHistory works properly', () => {
+    messagesController.showNotificationsHistory();
+    expect(console.log).toBeCalledWith(`${new Date().toJSON().slice(0,19).split('T').join(' ')} ${testMessage}`);
+  });
+
+});
