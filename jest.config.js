9a729e54ce5fb6394c732932f21a43af64564888
Artem Leonov-Dementiev <aleo@softserveinc.com>
2022-02-14 11:08:42 +0200

    Added vue test utils to the project

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json', 'vue'],
   transform: {
     '^.+\\.(js|jsx)?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+    '.*\\.(vue)$': 'vue3-jest'
+    
   },
   testEnvironment: 'node',
   moduleNameMapper: {
